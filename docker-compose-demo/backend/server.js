fc4f6f8a9ef4e3dd559ee508b80b5c5de732026d
TheDLCrimson <duclnc2004@gmail.com>
2025-07-22 23:41:56 +0700

    [TEST] Trying docker compose

---
+++ docker-compose-demo/backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Backend!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend running on http://localhost:${PORT}`);
+})
